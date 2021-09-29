7ec7c6335272ccabd7e7eae53c231592372f08bf
Zeteny100 <71275568+Zeteny100@users.noreply.github.com>
2021-09-29 21:14:10 +0200

    Add files via upload

---
+++ Házi feladat.py
@@ -0,0 +1,26 @@
+mat = int(input("add meg a matek átlagot: "))
+ang = int(input("add meg a angol átlagot: "))
+inf = int(input("add meg a matek átlagot: "))
+print((mat+ang+inf)/3)
+
+
+
+ido = int(input("Add meg az időt:"))
+mulva = int(input("Ennyi idő múlva szólaljon meg az ébresztő:"))
+print(ido+mulva)
+
+huf = int(input("Hány forintot ér az euró: "))
+mennyi = int(input("Ennyi eurót váltson fel: "))
+print(huf*mennyi)
+
+a = int(input("Add meg a téglalap a oldalát deciméterben: "))
+b = int(input("Add meg a téglalap b oldalát deciméterben: "))
+
+print("A téglalap kerülete cm-ben:", ((a*2)+(b*2))*10)
+print("A téglalap területe cm2-ben:", (a*b)*100)
+
+benzin = int(input("Hány liter benzin van a tankban: "))
+fogy = int(input("Az autó átlagos fogyasztása (L/100km): "))
+
+print(benzin/fogy)
+
